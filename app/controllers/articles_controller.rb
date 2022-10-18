1f37988a87542138b487e418615c6b4385f4f861
ganbari <steinway.nathan@gmail.com>
2022-10-18 18:48:56 -0500

    successfully generated article model and migrated to schemadb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
     def index
+        #Instructs active record model to return all items in database in the form of JSON
+        render json: Article.all
     end
 end
