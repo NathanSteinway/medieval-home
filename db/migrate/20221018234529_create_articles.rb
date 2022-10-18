9c8c92e8fc716a2d7eebcdc6f5cfbf032a548545
ganbari <steinway.nathan@gmail.com>
2022-10-18 18:51:14 -0500

    added comments

--- db/migrate/20221018234529_create_articles.rb
+++ db/migrate/20221018234529_create_articles.rb
@@ -1,5 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
+    # This has created the table below, which may now be dynamically migrated to the database allowing stable interaction
     create_table :articles do |t|
       t.string :title
       t.string :content
