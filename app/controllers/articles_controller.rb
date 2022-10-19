abfa6c6b9dad368184ed7438fd4207a16ad16f70
ganbari <steinway.nathan@gmail.com>
2022-10-18 19:13:15 -0500

    successfully retrieves data from the database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
     def index
         #Instructs active record model to return all items in database in the form of JSON
+        #Now that this is hooked up, this controller will return all items in the articles table.
+        #Since there is nothing in the database yet, this controller is returning an empty array.
         render json: Article.all
     end
 end
