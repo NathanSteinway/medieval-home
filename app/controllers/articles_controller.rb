2c3cb49556f9c00062a8d2bb8f53ee85f07ac801
ganbari <steinway.nathan@gmail.com>
2022-10-19 13:28:33 -0500

    staged POST logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,11 @@ class ArticlesController < ApplicationController
     # Here I am staging a POST request to the server so it may receive a payload representing an article
     # When complete this will insert the article into the SQLite database if valid, as well as return the new object's info in JSON format as specified in the assignment's README.md
     def create
-        # needs logic
+        Article.create(
+            title: 'Get Ready for the Big Chill', 
+            content: 'Winter is coming, and Americans may get a cold shock when they get their heating bills, according to a report released last week by the U.S. Energy Information Administration (EIA) that deserves more attention. Bundle up and set your thermostat at 68 degrees, or prepare to pay a bundle.',
+            author: 'The Editorial Board',
+            category: 'Opinion',
+            published_at: '2022-10-19T18:24:48+0000')
     end
 end
